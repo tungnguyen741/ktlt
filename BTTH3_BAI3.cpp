42e4870cff8987e384330051a6a933362c0d1c97
tungnguyen741 <57530101+tungnguyen741@users.noreply.github.com>
2019-11-22 01:00:50 +0700

    Add files via upload

---
+++ BTTH3_BAI3.cpp
@@ -0,0 +1,32 @@
+﻿////BAI TAP THUC HANH 3
+////Bài 3. Viết chương trình dùng cấp phát động để cho phép 2 con trỏ lưu trữ giá trị của 2 số
+////nguyên. Sau đó dựa trên địa chỉ mà con trỏ trỏ đến để tìm ước chung lớn nhất của 2 giá trị số
+////nguyên này bằng thuật giải Euclid
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int *a, *b;
+//	a = new int;
+//	b = new int;
+//	cout << "Nhap vao a: "; 
+//	cin >> *a;
+//	cout << "Nhap vao b: "; 
+//	cin >> *b;
+//	while(*b!=0)
+//	{
+//		int *c;
+//		c= new int;
+//		*c = *a % *b;
+//		*a = *b;
+//		*b = *c;
+//		delete c;
+//	}
+//	cout <<"UCLN cua a va b la: " << *a << endl;
+//	delete a;
+//	delete b;
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ BTTH3_BAI4.cpp
@@ -0,0 +1,61 @@
+﻿//Bài 4. Viết chương trình dùng cấp phát động, cho phép:
+//- Nhập vào 1 mảng 1 chiều lưu trữ số nguyên tối đa 30 phần tử.
+//- Dùng con trỏ và ký pháp độ dời để xuất ra các phần tử nào là số lẻ trong mảng.
+//- Trả về địa chỉ của giá trị phần tử lớn nhất trong mảng (giả sử các giá trị trong mảng không
+//trùng nhau)
+
+#include <iostream>
+using namespace std;
+
+#define MAXn 30
+
+int main()
+{
+	//cap phat mang
+		int *p = new int [MAXn];
+	int n;
+	cout << "Nhap vao so luong phan tu: "; cin >> n;
+	// nhap mang
+		for(int i = 0; i < n ; i++)
+		{
+			cout << "Nhap vao a[" << i << "]=" ;
+			cin >> *(p+i);
+		}
+
+
+	// xuat mang le
+	cout << "Cac phan tu le trong mang la: " ;
+		for(int i = 0; i < n; i++)
+		{
+			
+			if( *(p+i) % 2 !=0)
+				cout << *(p+i) <<" ";
+			cout << endl <<"vi tri la " << (p + i) << " " ;
+			
+		}
+		cout << endl;
+
+	//xuat mang
+	int	vtmax = 0;
+	int max = p[0];
+		for(int i = 0; i < n; i++)
+		{
+			if( max < *(p+i) )
+			{
+				max = *(p+i);
+				vtmax = i;
+			
+			}
+				cout << "vi tri lon " << vtmax << endl;
+			
+		}
+		cout << "dia chi cua gia tri lon nhat: " << (p + vtmax) << endl;
+	
+
+
+	//huy cap fat
+		delete []p;
+
+	system("pause");
+	return 0;
+}
